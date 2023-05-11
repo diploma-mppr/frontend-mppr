c3bcf5a0e44be2845e892681d30222b8a95d1951
yut_fut <seregey.golubev@mail.ru>
2023-05-11 22:40:17 +0300

    edited redirect pareto

--- src/components/Pareto.tsx
+++ src/components/Pareto.tsx
@@ -164,6 +164,7 @@ export const Pareto: React.FC = () => {
             console.log('success')
             const responseBody = await response.json();
             console.log(responseBody)
+            window.open("/")
         } else{
             console.log('prosas')
         }
